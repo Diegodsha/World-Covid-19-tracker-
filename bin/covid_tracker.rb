79d9aed7790a6f573d1567619506f148bbb197ea
Diego <diegodsha@hotmail.com>
2021-03-02 17:00:48 -0600

    Parsed page and count elements

--- bin/covid_tracker.rb
+++ bin/covid_tracker.rb
@@ -2,9 +2,21 @@ require 'httparty'
 require 'nokogiri'
 require 'byebug'
 
-class CovidTracker
-    def initialize
-        
-    end
+
+#class CovidTracker
+def countries
+#def initialize
+    url = "https://www.worldometers.info/coronavirus/"
+    unparsed_page = HTTParty.get(url)
+    parsed_page = Nokogiri::HTML(unparsed_page)
     
+    all_countries = parsed_page.css('table#main_table_countries_today > tbody > tr').count
+    
+
+
+
 end
+
+#countries
+
+#end
